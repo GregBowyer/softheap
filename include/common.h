844ccb1b0bfe4837141579f629abbb21d8d6cb8f
Shaun Verch <sverch@urx.com>
2014-09-09 15:51:17 -0400

    Add backtrace to ensure

--- include/common.h
+++ include/common.h
@@ -6,14 +6,29 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <execinfo.h>
+
+// TODO: Move this to a better place
+static inline void printBacktrace() {
+    void* tracePtrs[100];
+    int count = backtrace(tracePtrs, 100);
+    char** funcNames = backtrace_symbols(tracePtrs, count);
+
+    for (int i = 0; i < count; i++) {
+        printf("%s\n", funcNames[i]);
+    }
+
+    free(funcNames);
+}
 
 // ensure is kinda like assert but it is always executed
 #define ensure(p, msg)                                   \
 do {                                                     \
     if (!(p)) {                                          \
-        printf("%s - %s:%d", msg, __FILE__, __LINE__);   \
+        printf("%s - %s:%d\n", msg, __FILE__, __LINE__);   \
+        printBacktrace();                                \
         abort();                                         \
     }                                                    \
 } while(0)
 
-#endif 
+#endif
